174115215585be9999d9df2114353dcaa933447b
Neal Conway <conwanea@gmail.com>
2020-07-17 06:47:08 -0500

    added moon and homepage link

--- mst3k/src/App.js
+++ mst3k/src/App.js
@@ -6,10 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://dr56wvhu2c8zo.cloudfront.net/mst3k/assets/a75194ad-b8c2-41f9-a334-fa5333bd7a0f/mst3k-moon-official.png"
+          className="App-logo"
+          alt="logo"
+        />
         <p>The begining of my Mystery Scince Theater 3000 database</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+        <a className="App-link" href="https://mst3k.com/" target="_blank" rel="noopener noreferrer">
+          Visit the MST3K Homepage!
         </a>
       </header>
     </div>
